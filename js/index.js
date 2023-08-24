cf1e51f4f763c636c8059f89200b0051272106a7
Jackspence6 <jackspence123456@gmail.com>
2023-08-24 12:00:03 +0200

    Added README template to const userReadmeData.

--- js/index.js
+++ js/index.js
@@ -4,6 +4,8 @@
 // Importing "Inquirer" to application
 import { error, log } from "console";
 import inquirer from "inquirer";
+// Importing "fs" to application
+import fs from "fs";
 /******************************************/
 /* Environment Variables and Constants */
 /******************************************/
@@ -43,40 +45,40 @@ import inquirer from "inquirer";
 inquirer
   .prompt([
     {
-      type: "Input",
+      type: "input",
       name: "projectProfileUrl",
       message: "Please provide your GitHub Url:",
     },
     {
-      type: "Input",
+      type: "input",
       name: "projectTitle",
       message: "Please provide you project title:",
     },
     {
-      type: "Input",
+      type: "input",
       name: "projectDescription",
       message: "Please provide a brief description of your project:",
     },
     {
-      type: "Input",
+      type: "input",
       name: "projectFeatures",
       message:
         "Please provide you a list of your project's Features:(Separate each feature with a semi-colon)",
     },
     {
-      type: "Input",
+      type: "input",
       name: "projectInstallation",
       message:
         "Please provide the steps required to install your application: (Separate each step with a semi-colon)",
     },
     {
-      type: "Input",
+      type: "input",
       name: "projectUsage",
       message:
         "Please provide the usage steps for your application: (Separate each step with a semi-colon)",
     },
     {
-      type: "Input",
+      type: "input",
       name: "projectCredits",
       message:
         "Please provide a list of people who contributed to your project, if any: (Separate each contributor with a semi-colon)",
@@ -89,8 +91,8 @@ inquirer
         "Apache License 2.0",
         "GNU General Public License v3.0",
         "MIT License",
-        'BSD 2-Clause "Simplified" License',
-        'BSD 3-Clause "New" or "Revised" License',
+        "BSD 2-Clause 'Simplified' License",
+        "BSD 3-Clause 'New' or 'Revised' License",
         "Boost Software License 1.0",
         "Creative Commons Zero v1.0 Universal",
         "Eclipse Public License 2.0",
@@ -102,14 +104,39 @@ inquirer
       ],
     },
     {
-      type: "Input",
+      type: "input",
       name: "projectEmail",
       message: "Please your Email Address:",
     },
   ])
   .then((answers) => {
     console.log(answers);
-  })
-  .catch((error) => {
-    console.log("Please Try Again");
+    // Adding user data to README
+    const userReadmeData = `
+# ${answers.projectTitle}
+${answers.projectDescription}
+
+## Table of Contents
+
+- [Installation](#Installation)
+- [Usage](#usage)
+- [Features](#features)
+- [How to Contribute](#how-to-contribute)
+- [Credits](#credits)
+- [License](#License)
+
+
+## Installation
+
+## Usage
+
+## Features
+
+## Contributing
+
+## Credits
+
+## License
+
+`;
   });
